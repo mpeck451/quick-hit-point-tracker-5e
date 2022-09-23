a8e0ff422620a4ea7bb791d3cd05ca3e4edb061f
Mason Peck <mpeck451@outlook.com>
2022-09-23 15:03:46 -0500

    Add handlers for death saving throw buttons.

--- client/src/death-saving-throws-tracker.js
+++ client/src/death-saving-throws-tracker.js
@@ -7,34 +7,51 @@ export function DeathSavingThrowsTracker({
     {
 
     const successes = Number(playerProfile.deathSavingThrowSuccess);
-    const failure = Number(playerProfile.deathSavingThrowFailure);
+    const failures = Number(playerProfile.deathSavingThrowFailure);
 
-    const deathSavingThrowStatus = (type, number) => {
-        
+    const handleStatusChange = (type, operator) => {
+        type = 'deathSavingThrow' + type;
+        setPlayerProfile((prev) => ({
+            ...prev,
+            [type]: operator === 'add' ? playerProfile[type] + 1 : playerProfile[type] - 1,
+        }));
+    }
+
+    const savingThrowStatus = (type, number) => {
+        let status = (type === "Success" ? successes: failures) >= number ? type : null;
+        switch (status) {
+            case 'Success': return 'glyphicon glyphicon-ok'
+            case 'Failure': return 'glyphicon glyphicon-remove'
+            default: return 'glyphicon glyphicon-unchecked';
+        }
     }
 
     return (
         <div>
             <h2>Death Saving Throws</h2>
             <label>Successes: &nbsp;
-                <span className='glyphicon glyphicon-unchecked'></span> &nbsp;
-                <span className='glyphicon glyphicon-unchecked'></span> &nbsp;
-                <span className='glyphicon glyphicon-unchecked'></span> &nbsp;
-                <button
-                    className={dynamicStyle('button')}><span className="glyphicon glyphicon-plus"></span></button>
-                <button
-                    className={dynamicStyle('button')}><span className="glyphicon glyphicon-minus"></span></button>
+                <span className={savingThrowStatus('Success', 1)}></span> &nbsp;
+                <span className={savingThrowStatus('Success', 2)}></span> &nbsp;
+                <span className={savingThrowStatus('Success', 3)}></span> &nbsp;
             </label>
+                <button
+                    className={dynamicStyle('button')}
+                    onClick={() => handleStatusChange('Success', 'add')}><span className="glyphicon glyphicon-plus"></span></button>
+                <button
+                    className={dynamicStyle('button')}
+                    onClick={() => handleStatusChange('Success', 'subtract')}><span className="glyphicon glyphicon-minus"></span></button>
             <br />
             <label>Failures: &nbsp;
-                <span className='glyphicon glyphicon-unchecked'></span> &nbsp;
-                <span className='glyphicon glyphicon-unchecked'></span> &nbsp;
-                <span className='glyphicon glyphicon-unchecked'></span> &nbsp;
-                <button
-                    className={dynamicStyle('button')}><span className="glyphicon glyphicon-plus"></span></button>
-                <button
-                    className={dynamicStyle('button')}><span className="glyphicon glyphicon-minus"></span></button>
+                <span className={savingThrowStatus('Failure', 1)}></span> &nbsp;
+                <span className={savingThrowStatus('Failure', 2)}></span> &nbsp;
+                <span className={savingThrowStatus('Failure', 3)}></span> &nbsp;
             </label>
+                <button
+                    className={dynamicStyle('button')}
+                    onClick={() => handleStatusChange('Failure', 'add')}><span className="glyphicon glyphicon-plus"></span></button>
+                <button
+                    className={dynamicStyle('button')}
+                    onClick={() => handleStatusChange('Failure', 'subtract')}><span className="glyphicon glyphicon-minus"></span></button>
             <br />
             <button
                 className={dynamicStyle('button')}
